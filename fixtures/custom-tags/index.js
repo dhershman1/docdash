05968865274279ab5d10ca0f81ea38651bdfc166
dhershman1 <dustinh17@gmail.com>
2018-11-21 12:32:40 -0500

    Fixed signature rendering and added test file

---
+++ fixtures/custom-tags/index.js
@@ -0,0 +1,16 @@
+exports.defineTags = function (dictionary) {
+  dictionary.defineTag('category', {
+    mustHaveValue: true,
+    onTagged (doclet, tag) {
+      doclet.category = tag.value
+    }
+  })
+  dictionary.defineTag('sig', {
+    mustHaveValue: true,
+    onTagged (doclet, tag) {
+      doclet.sig = tag.value.split(/\r|\n|\n\r/g)
+
+      console.log(doclet.sig)
+    }
+  })
+}
